3cc7ea912d9dc9991d4b161b48a0cc36797210ac
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2026-05-05 10:12:12 +0530

    day37.c

---
+++ day37.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[1000];
+    int size=0;
+    for(int i=0;i<n;i++)
+    {
+        char op[10];
+        scanf("%s",op);
+        if(strcmp(op,"insert")==0)
+        {
+            int x;
+            scanf("%d",&x);
+            a[size++]=x;
+        }
+        else if(strcmp(op,"delete")==0)
+        {
+            if(size==0)
+            {
+                printf("-1\n");
+            }
+            else
+            {
+                int min=0;
+                for(int j=1;j<size;j++)
+                {
+                    if(a[j]<a[min])
+                    {
+                        min=j;
+                    }
+                }
+                printf("%d\n",a[min]);
+                for(int j=min;j<size-1;j++)
+                {
+                    a[j]=a[j+1];
+                }
+                size--;
+            }
+        }
+        else if(strcmp(op,"peek")==0)
+        {
+            if(size==0)
+            {
+                printf("-1\n");
+            }
+            else
+            {
+                int min=0;
+                for(int j=1;j<size;j++)
+                {
+                    if(a[j]<a[min])
+                    {
+                        min=j;
+                    }
+                }
+                printf("%d\n",a[min]);
+            }
+        }
+    }
+    return 0;
+}
