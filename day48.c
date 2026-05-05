eb31e52635093d74d9a1b354f991858226e153e7
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2026-05-05 10:19:18 +0530

    day48.c

---
+++ day48.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* newnode(int val)
+{
+    struct node* n=(struct node*)malloc(sizeof(struct node));
+    n->data=val;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+
+int height(struct node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    int l=height(root->left);
+    int r=height(root->right);
+    return (l>r?l:r)+1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(n==0||a[0]==-1)
+    {
+        printf("0");
+        return 0;
+    }
+    struct node* nodes[n];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==-1)
+        {
+            nodes[i]=NULL;
+        }
+        else
+        {
+            nodes[i]=newnode(a[i]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(nodes[i]!=NULL)
+        {
+            int l=2*i+1;
+            int r=2*i+2;
+            if(l<n)
+            {
+                nodes[i]->left=nodes[l];
+            }
+            if(r<n)
+            {
+                nodes[i]->right=nodes[r];
+            }
+        }
+    }
+    printf("%d",height(nodes[0]));
+    return 0;
+}
