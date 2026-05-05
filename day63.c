4695874081fd8ea09d87f49cf1ce9e01e4426005
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2026-05-05 10:38:47 +0530

    day63.c

---
+++ day63.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+void dfs(int v,struct node* adj[],int visited[])
+{
+    visited[v]=1;
+    printf("%d ",v);
+    struct node* temp=adj[v];
+    while(temp!=NULL)
+    {
+        if(!visited[temp->data])
+        {
+            dfs(temp->data,adj,visited);
+        }
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+
+    struct node* adj[n];
+    for(int i=0;i<n;i++)
+    {
+        adj[i]=NULL;
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        scanf("%d %d",&u,&v);
+
+        struct node* newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->data=v;
+        newnode->next=adj[u];
+        adj[u]=newnode;
+
+        struct node* newnode2=(struct node*)malloc(sizeof(struct node));
+        newnode2->data=u;
+        newnode2->next=adj[v];
+        adj[v]=newnode2;
+    }
+
+    int s;
+    scanf("%d",&s);
+
+    int visited[n];
+    for(int i=0;i<n;i++)
+    {
+        visited[i]=0;
+    }
+
+    dfs(s,adj,visited);
+
+    return 0;
+}
